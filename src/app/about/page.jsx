40dfd8f52aade4bf67b30011da9714b51d698fb6
Samagra Khanna <85937988+samagra-khanna@users.noreply.github.com>
2023-03-19 21:12:32 +0530

    Add loading effect

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,6 @@
 export default function page() {
     return (
-        <div>
+        <div className="mt-4">
             <h2 className="text-2xl ml-3 font-bold text-amber-400 border-b-[3px] border-amber-400 inline py-[2px] px-[2px]">About</h2>
             <span className="ml-1 flex items-center mt-6 font-mono text-lg max-w-2xl px-2">Hello, this is not an official IMDb website, and just a clone website created by Samagra Khanna to learn Next.js and for personal usage, though it is free for everyone to use if they want to.</span>
         </div>
---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function loading() {
+    return (
+        <div className="fixed w-full h-full top-0 left-0 right-0 bottom-0 bg-white dark:bg-[#000000] flex justify-center items-center">
+            <img className="h-[70px]" src="spinner.svg" alt="loading" />
+        </div>
+    );
+};
